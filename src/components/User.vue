5bdf8d337f1619afa870a0d5315c316590a5c376
조승희 <shshy81@gmail.com>
2022-07-23 17:42:16 +0900

    init

---
+++ src/components/User.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>User 컴포넌트</h1>
+    <p>이름 : {{ name }}</p>
+    <button @click="changeName()">이름 변경</button>
+    <hr>
+    <UserDetail :nameOfChild="name"></UserDetail>
+    <UserEdit></UserEdit>
+  </div>
+</template>
+
+<script>
+import UserDetail from './UserDetail'
+import UserEdit from './UserEdit'
+import { ref } from 'vue'
+
+export default {
+  components: {
+    UserDetail,
+    UserEdit
+  },
+  setup() {
+    const name =  ref('뷰제이에스'),
+    changeName = () => {
+        name.value = 'cho'
+    }
+    return {
+    name,
+    changeName
+  }
+  },
+  
+}
+</script>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="red lighten-3 pa-3">
+    <h3>자세한 회원 정보를 확인합니다.</h3>
+    <p>{{ nameOfChild }}</p>
+    <p>{{ sayHello }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        // 부모로부터 받기때문에 값을 넣는게 아닌 타입 선언
+        nameOfChild: {
+            type: String,
+            // required: true,
+            default: 'aaa'
+        }
+    },
+    computed: {
+        sayHello() {
+            return this.nameOfChild + ' Hi~~!!'
+        }
+    },
+}
+</script>
